254a8be9fb4d178ad08fae49d0c03aa62fcc9900
Brad Martin <bradwaynemartin@gmail.com>
2021-10-07 11:13:42 -0500

    fix android types (#4)

--- packages/payments/order/order.common.ts
+++ packages/payments/order/order.common.ts
@@ -1,24 +1,21 @@
-type Purchase = com.android.billingclient.api.Purchase;
-type PurchaseHistoryRecord = com.android.billingclient.api.PurchaseHistoryRecord;
-
 export abstract class BaseOrder {
-	public nativeValue: Purchase | PurchaseHistoryRecord | SKPaymentTransaction;
+	public nativeValue: com.android.billingclient.api.Purchase | com.android.billingclient.api.PurchaseHistoryRecord | SKPaymentTransaction;
 	public abstract readonly debug: string | null;
 
-  public state: OrderState;
-  public itemId: string;
-  public orderId: string | null;
-  public orderDate: Date;
-  public receiptToken: string;
-  public userData: string;
-  public restored: boolean;
-  /** Android only */
-  public isSubscription: boolean;
-  public dataSignature: string;
-  public acknowledged: boolean;
-  public quantity: number;
+	public state: OrderState;
+	public itemId: string;
+	public orderId: string | null;
+	public orderDate: Date;
+	public receiptToken: string;
+	public userData: string;
+	public restored: boolean;
+	/** Android only */
+	public isSubscription: boolean;
+	public dataSignature: string;
+	public acknowledged: boolean;
+	public quantity: number;
 
-	constructor(nativeValue: Purchase | PurchaseHistoryRecord | SKPaymentTransaction, restored: boolean = false) {
+	constructor(nativeValue: com.android.billingclient.api.Purchase | com.android.billingclient.api.PurchaseHistoryRecord | SKPaymentTransaction, restored: boolean = false) {
 		this.nativeValue = nativeValue;
 		this.restored = restored;
 	}
--- packages/payments/order/order.d.ts
+++ packages/payments/order/order.d.ts
@@ -1,11 +1,9 @@
 import { BaseOrder } from './order.common';
-declare type Purchase = com.android.billingclient.api.Purchase;
-declare type PurchaseHistoryRecord = com.android.billingclient.api.PurchaseHistoryRecord;
 
 export { OrderState } from './order.common';
 
 export declare class Order extends BaseOrder {
 	public readonly debug: string | null;
 
-	constructor(nativeValue: Purchase | PurchaseHistoryRecord | SKPaymentTransaction, restored?: boolean);
+	constructor(nativeValue: com.android.billingclient.api.Purchase | com.android.billingclient.api.PurchaseHistoryRecord | SKPaymentTransaction, restored?: boolean);
 }
