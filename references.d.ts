4c7d9dbf8ffe90c1dac49002fe2d3d230eedcc21
Nathan Walker <walkerrunpdx@gmail.com>
2022-07-04 10:05:18 -0700

    chore: use PassKit ios framework types

--- references.d.ts
+++ references.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
 /// <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!PassKit.d.ts" />
